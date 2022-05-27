7b0227bcb5c864acfc3de35cf648002d85edc271
Akshay Shinde <akshayshinde7076@gmail.com>
2022-05-27 20:28:41 +0530

    updated

---
+++ Demo.java
@@ -0,0 +1,22 @@
+class Demo
+{
+	public static void main(String args[])
+	{
+		int a, b, sum;
+		a= 1000;
+		b= 2000;
+		sum= a + b;
+		int sub= a - b;
+		a+=a;
+		b+=b;
+		int div= b/a;
+		
+	System.out.println(sum);
+	System.out.println(sub);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(div);
+	System.out.println("Akshay Shinde");
+	System.out.println("Hellllo");
+	}
+}
